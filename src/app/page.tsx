01bbf4d165e75f3c4b0a6d9d6950d3f36295181e
JediYoshi <jediyoshi@proton.me>
2025-09-15 10:39:46 -1000

    Test 3 Images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+	"https://07be8fxoe9.ufs.sh/f/7irEBIre2xe9HjZ5iXnJaOhm92li1bJcQUBTtG6Dk4qM3n5o",
+	"https://07be8fxoe9.ufs.sh/f/7irEBIre2xe9aQzuJKbrmxil56zMSZC1Q0TdARw4P3toespV",
+	"https://07be8fxoe9.ufs.sh/f/7irEBIre2xe9m70NNeOatIxNk4AYForyJdMb3WBjHOQZD2zh"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
+
 export default function HomePage() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-				<h1 className="font-extrabold text-5xl text-white tracking-tight sm:text-[5rem]">
-					Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-				</h1>
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/usage/first-steps"
-						target="_blank"
-					>
-						<h3 className="font-bold text-2xl">First Steps →</h3>
-						<div className="text-lg">
-							Just the basics - Everything you need to know to set up your
-							database and authentication.
-						</div>
-					</Link>
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/introduction"
-						target="_blank"
-					>
-						<h3 className="font-bold text-2xl">Documentation →</h3>
-						<div className="text-lg">
-							Learn more about Create T3 App, the libraries it uses, and how to
-							deploy it.
-						</div>
-					</Link>
-				</div>
-			</div>
+		<main className="">
+			<div className="flex flex-wrap gap-4">{
+				mockImages.map((image) => (
+					<div key={image.id} className="w-48">
+						<img src={image.url} alt="image" />
+					</div>
+				))
+			}</div>
 		</main>
 	);
 }
